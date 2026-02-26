6ec34c8046435abf0a61bc059a1544affdbcaa60
alejoceballos <alejoceballos75@gmail.com>
2026-02-26 00:13:07 +0000

    Add UI service container

--- dungeon-walker-docker/build-n-run.sh
+++ dungeon-walker-docker/build-n-run.sh
@@ -3,13 +3,17 @@
 docker container stop $(docker ps -a -q)
 docker container rm $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
-docker image rm alejoceballos/dungeon-walker-engine:v1
 docker image rm alejoceballos/dungeon-walker-ws-server:v1
+docker image rm alejoceballos/dungeon-walker-engine:v1
+docker image rm alejoceballos/dungeon-walker-ui:v1
 
 cd ../dungeon-walker-engine || exit
 mvn clean install jib:dockerBuild -U -DskipTests
 
 cd ../dungeon-walker-ws-server || exit
+mvn clean install jib:dockerBuild -U -DskipTests
+
+cd ../dungeon-walker-ui || exit
 mvn clean install jib:dockerBuild -U -DskipTests
 
 cd ../dungeon-walker-docker || exit
